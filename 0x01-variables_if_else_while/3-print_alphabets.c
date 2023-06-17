4ddae64e66e436b339181aaf66c2068952637072
Bridgeteh <honyenugabri@gmail.com>
2023-06-17 15:24:51 -0700

    gf

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include"stdio.h"
+/**
+ * main - Prints both kinds of alphabet.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char bridg[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
+	int size;
+	
+	size = sizeof(bridg) / sizeof(bridg[0]) - 1;
+	for (i = 0; i < size; i++)
+		putchar(dave[i]);
+	putchar('\n');
+	return (0);
+}
