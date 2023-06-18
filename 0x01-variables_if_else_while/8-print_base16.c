1005dacc3efe5bce8ef2e1e6f1d123c4f21416fa
Bridgeteh <honyenugabri@gmail.com>
2023-06-18 10:18:41 -0700

    ds

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include"stdio.h"
+/**
+ * main - Prints numbers between 0 to 9 and letters between a to f.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int t;
+
+	for (t = 48; t < 58; t++)
+		putchar(t);
+	for (t = 97; t < 103; t++)
+		putchar(t);
+	putchar('\n');
+	return (0);
+}
