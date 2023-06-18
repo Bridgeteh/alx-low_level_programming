36bc9ca4454884a4d765fd9fbd1d341f41060f42
Bridgeteh <honyenugabri@gmail.com>
2023-06-18 10:22:41 -0700

    ds

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,7 +6,7 @@
 */
 int main(void)
 {
-	char bridg[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char bridg [] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	int i;
 	int size;
 	
