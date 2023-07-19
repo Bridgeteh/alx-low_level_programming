9f4e3b30186c7cf3d81db9a2e5d19f815be2ac18
Bridgeteh <honyenugabri@gmail.com>
2023-07-18 17:00:20 -0700

    File name

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,6 +1,13 @@
-#ifndef MAIN_C
-#define MAIN_C 
+#include <stdio.h>
 
-#define FILENAME 
+/**
+ * main - prints the name of the file
+ *
+ * Return: Always 0 (Success)
+ */
+int ain(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
 
-#endif
